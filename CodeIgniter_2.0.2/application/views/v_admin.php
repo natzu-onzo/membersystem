76f369ba6c7a9e18be01cad373de5b8814c18667
albergren <martin@ansuz.net>
2017-05-10 10:44:52 +0200

    Tab layout in admin

--- CodeIgniter_2.0.2/application/views/v_admin.php
+++ CodeIgniter_2.0.2/application/views/v_admin.php
@@ -5,11 +5,14 @@
 	<title><?php echo $title;?></title>
 
 <link rel="stylesheet" href="<?php echo base_url(); ?>ressources/kbhff_2012.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/ressources/tab_layout.css" type="text/css" />
+
 <?php echo isset($library_src) ? $library_src : ''; ?>
 <script type="text/javascript" charset="utf-8" src="/ressources/jquery.form.js"></script>
 <script type="text/javascript" src="/ressources/jquery/jquery.datepick.js"></script>
 <script type="text/javascript" src="/ressources/jquery/jquery.datepick-da.js"></script>
 <link rel="STYLESHEET" type="text/css" href="/ressources/1st.datepick.css">
+
 <script language="JavaScript" type="text/javascript">
 $(function() {
 	$('#dato').datepick({ dateFormat: 'yyyy-mm-dd' });
@@ -42,18 +45,58 @@ F&Oslash;DEVAREF&AElig;LLESSKAB <span id="green">/ MEDLEMSSYSTEM</span></span>
 		echo ('<!--' . $debug . '-->');
 	}
 ?>
+
+<div id="tabs" class="c-tabs no-js">
+    <div class="c-tabs-nav">
+        <a href="#" class="c-tabs-nav__link is-active">Medlemmer</a>
+        <a href="#" class="c-tabs-nav__link">Afhentningsdage</a>
+        <a href="#" class="c-tabs-nav__link">Løssalg</a>
+        <a href="#" class="c-tabs-nav__link">Salg</a>
+</div>
+
+
+<!-- MEDLEMMER -->
+<div class="c-tab is-active">
+<div class="c-tab__content">                                                                                                                                  
 <?php echo $content;?>
 Hent Excel-liste over medlemmer:<br>
 <?php echo $excelsel ?>
 <br>
-Registrer kontant-ordrer:<br>
-<?php echo $cashsel ?>
-<br>
 
+<a href="/mail">Massemail</a><br>
+<br>
+Rediger medlemmer<br>
+<form action="/admin/medlemmer/" method="post">
+Afdeling: <select name="division">
+<?php echo $createsel ?>
+</select>
+<input type="submit" value="Rediger" class="form_button"><br>
+</form>
+<br>
+Rediger tekst i brev til nye medlemmer:<br>
+<?php echo $welcome ?>
+<br>
+Mail til nye medlemmer:<br>
+<form action="/admin/nyemedlemmer/" method="post">
+Afdeling: <select name="division"><option value="0">Alle afdelinger</a>
+<?php echo $createsel ?>
+</select>
+siden: <input type="text" name="dato" id="dato4" size="10" maxlength="10">&nbsp;<input type="submit" value="Vis" class="form_button"><br>
+</form>
+                                   
 <!--
-<?php echo($createsel); ?>
+Se et medlems ordrer<br>
+<form action="/minside/mine_ordrer/" method="post">
+Medlemsnummer: <input type="text" name="id"> <input type="submit" value="Se medlems ordrer" class="form_button"><br>
+</form>
 -->
-    
+</div>
+</div>
+
+<!-- AFHENTNINGSDAGE -->
+<div class="c-tab">
+<div class="c-tab__content">
+
 Se afhentningsdage:<br>
 <form action="/admin/liste/" method="post">
 Afdeling: <select name="division">
@@ -87,45 +130,84 @@ Dag: <input type="text" name="dato" id="dato" size="10" maxlength="10"> Sidste o
 	
 	}
 ?>
+</div>
+</div>
+
+
+<!-- LØSSALG -->
+<div class="c-tab">
+<div class="c-tab__content">
+Opret ny løssalgsvare: <br>
+<?php
+    {
+        echo '<form action="#" method="post">' ."\n";
+        echo 'Type: <input type="text" name="type" id="type" size="10" maxlength="10">';
+		echo '<input type="submit" value="Opret" class="form_button"><br>' ."\n";
+
+		echo '</form>' ."\n";
+    }
+?>
+
+<br> <br>
+Tilføj løssalgvarer til afhentningsdag: <br>
+<?php
+	{
+		echo 'Vælg afhentningsdag:<br>'."\n";
+		echo '<form action="/admin/opretf/' . $bagday['id'] . '" method="post">' ."\n";
+		echo 'Dag: <select name="pickupday">'."\n";
+		echo $createfsel;
+		echo '</select> <br><br>'."\n";
+        echo 'Vælg vare:'."\n";
+        echo '<select name="lossalgvare">' ."\n";
+        echo '<option value="honning">Honning</option>'."\n";
+        echo '</select> <br><br>'."\n";
+		echo 'Sidste ordre: <input type="text" name="dato'.$bagday['id'].'" 
+              id="dato'.$bagday['id'].'" size="10" maxlength="10"> 
+              <input type="text" name="tid'.$bagday['id'].'" id="tid'.$bagday['id'].'" 
+              value="18:30" size="5" maxlength="5">
+              <input type="submit" value="Opret" class="form_button"><br>' ."\n";
+		echo '</form>' ."\n";
+		echo '<br>' ."\n";
+	
+	
+	}
+?>
+</div>
+</div>
+
+
+<!-- SALG -->
+<div class="c-tab">
+<div class="c-tab__content">
+Registrer kontant-ordrer:<br>
+<?php echo $cashsel ?>
+
 <br>
 Dagens salg:<br>
 <?php echo $dagenssalg ?>
 <br>
-Mail til nye medlemmer:<br>
-<form action="/admin/nyemedlemmer/" method="post">
-Afdeling: <select name="division"><option value="0">Alle afdelinger</a>
-<?php echo $createsel ?>
-</select>
-siden: <input type="text" name="dato" id="dato4" size="10" maxlength="10">&nbsp;<input type="submit" value="Vis" class="form_button"><br>
-</form>
+
 <br>
 Statistik over ikke-afhentede poser:<br>
 <a href="/admin/ikke_afhentet">Ikke-afhentede poser</a><br>
-<br>
-<a href="/mail">Massemail</a><br>
-<br>
-Rediger medlemmer<br>
-<form action="/admin/medlemmer/" method="post">
-Afdeling: <select name="division">
-<?php echo $createsel ?>
-</select>
-<input type="submit" value="Rediger" class="form_button"><br>
-</form>
-<br>
-Rediger tekst i brev til nye medlemmer:<br>
-<?php echo $welcome ?>
-<br>
+</div>
+</div>
 
-<!--
-Se et medlems ordrer<br>
-<form action="/minside/mine_ordrer/" method="post">
-Medlemsnummer: <input type="text" name="id"> <input type="submit" value="Se medlems ordrer" class="form_button"><br>
-</form>
--->
 <br>
 </span>
 <hr align="left" id="bottomhr">
 <?php echo isset($script_head) ? $script_head : ''; ?>
 <?php echo isset($script_foot) ? $script_foot : ''; ?>
+
+<script src="/ressources/tabs.js"></script>
+<script>
+    var myTabs = tabs({
+    el: '#tabs',
+    tabNavigationLinks: '.c-tabs-nav__link',
+    tabContentContainers: '.c-tab'
+    });
+    myTabs.init();      
+</script>
+
 </body>
 </html>
