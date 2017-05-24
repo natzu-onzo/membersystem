2ca7ca9154ab5f1fc5a3a9b7013cc49010a54f0d
albergren <martin@ansuz.net>
2017-05-24 13:35:01 +0200

    add table ff_product

--- kbhff.sql
+++ kbhff.sql
@@ -12878,7 +12878,9 @@ CREATE TABLE IF NOT EXISTS `ff_product_details` (
 CREATE TABLE IF NOT EXISTS `ff_products` (
   `product_id` int unsigned NOT NULL AUTO_INCREMENT,
   `id` mediumint(9) unsigned NOT NULL,
-  `description` TEXT
+  `description` TEXT,
+  PRIMARY KEY (product_id),
+  FOREIGN KEY (id) REFERENCES ff_producttypes (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci AUTO_INCREMENT=1 ;
 
 --
